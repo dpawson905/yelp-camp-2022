5f8f5054d847138ac9d958e8ef4cf1577374c12f
Darrell Pawson <dpawson905@gmail.com>
2022-04-14 21:01:49 -0400

    add pagination

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate-v2');
 const Review = require('./review')
 const Schema = mongoose.Schema;
 
@@ -63,5 +64,5 @@ CampgroundSchema.post('findOneAndDelete', async function (doc) {
         })
     }
 })
-
+CampgroundSchema.plugin(mongoosePaginate);
 module.exports = mongoose.model('Campground', CampgroundSchema);
