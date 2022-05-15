ab6f32284fbf3471a41dd8b19fbf980f6e0d75b4
Darrell Pawson <dpawson905@gmail.com>
2022-05-14 23:57:04 -0400

    refactor

---
+++ utils/corsHelper.js
@@ -0,0 +1,37 @@
+exports.scriptSrcUrls = () => {
+  return [
+    "https://api.tiles.mapbox.com",
+    "https://api.mapbox.com",
+    "https://kit.fontawesome.com",
+    "https://cdnjs.cloudflare.com",
+    "https://cdn.jsdelivr.net",
+  ];
+};
+
+exports.styleSrcUrls = () => {
+  return [
+    "https://kit-free.fontawesome.com",
+    "https://cdn.jsdelivr.net",
+    "https://api.mapbox.com",
+    "https://api.tiles.mapbox.com",
+    "https://fonts.googleapis.com",
+    "https://use.fontawesome.com",
+  ];
+};
+
+exports.connectSrcUrls = () => {
+  return [
+    "https://api.mapbox.com",
+    "https://*.tiles.mapbox.com",
+    "https://events.mapbox.com",
+  ];
+};
+exports.fontSrcUrls = () => {
+  return [];
+};
+exports.imgSrcUrls = () => {
+  return [
+    "https://res.cloudinary.com/campcloud/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+    "https://images.unsplash.com",
+  ];
+};
