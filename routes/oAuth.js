8bb3ef452d18281031993dca0071fd011f7fa4a0
Darrell Pawson <dpawson905@gmail.com>
2022-05-29 12:45:23 -0400

    add oAuth

---
+++ routes/oAuth.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const catchAsync = require("../utils/catchAsync");
+const oAuthController = require("../controllers/oAuth");
+const passport = require("passport");
+
+router.get("/google", oAuthController.loadGoogleLogin);
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/campgrounds",
+    failureFlash: true,
+    successFlash: true,
+  }),
+  catchAsync(oAuthController.googleRegisterOrLogin)
+);
+
+module.exports = router;
